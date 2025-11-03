cd61e46f284ed81c98837c5ff4262ed6510b91d3
SANDE <fsande@bouyguestelecom-solution.fr>
2025-11-03 17:55:22 +0100

    correction cache

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            $appUrl = config('app.url');
+            if (!empty($appUrl)) {
+                URL::forceRootUrl($appUrl);
+            }
+            if (str_starts_with((string) $appUrl, 'https://') || (bool) env('APP_FORCE_HTTPS', false)) {
+                URL::forceScheme('https');
+            }
+        }
     }
 }
